d0f1074cfb12212c6edc6d3eb639c27e6ed358dc
Brian Birdwell <bbirdwell@gmail.com>
2018-10-01 06:52:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,8 @@ def prime?(num)
   array = (2..(num-1)).to_a
   if array.none? { |n| num % n == 0 || n == 1 || n == 2}
     return true
-  elsif array.any? { |n| num % n == 0 } 
+  elsif array.any? { |n| num % n == 0 } || -num > 0
     return false
-  elsif num * -1 > 0
-  return false
   end
 
 end
